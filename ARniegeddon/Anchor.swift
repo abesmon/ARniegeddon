85c5c48d8e5d7ed4a587929d1c1bb07a484c9243
Alexey Lysenko <abesmon@gmail.com>
2021-06-17 13:34:31 +0300

    initial

---
+++ ARniegeddon/Anchor.swift
@@ -0,0 +1,13 @@
+//
+//  Anchor.swift
+//  ARniegeddon
+//
+//  Created by Алексей Лысенко on 17.06.2021.
+//  Copyright © 2021 Ray Wenderlich. All rights reserved.
+//
+
+import ARKit
+
+class Anchor: ARAnchor {
+  var type: NodeType?
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#  ARKit bug destroyer
+
+This is example AR kit made with ray wanderlich tutorial https://www.raywenderlich.com/378-augmented-reality-and-arkit-tutorial
